57cee53efa304aae180ae1298dc57648c054d2e4
Nikita Shishin <nshishin@insys.io>
2019-05-07 15:55:17 +0600

    obs concatination

--- swapi-app/src/app/components/swapi-dashboard/swapi-dashboard.component.ts
+++ swapi-app/src/app/components/swapi-dashboard/swapi-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SwapiService } from 'src/app/services/swapi.service';
-import { Observable } from 'rxjs';
+import { Observable, pipe, of, concat } from 'rxjs';
 import { SwapiRoot } from 'src/app/models/swapi-root';
 import { SwapiPeople } from 'src/app/models/swapi-people';
 import { SwapiCollectionResult } from 'src/app/models/swapi-collection-result';
+import { concatAll } from 'rxjs/operators';
 // import { SwapiService } from 'ng2-swapi';
 
 @Component({
@@ -13,9 +14,10 @@ import { SwapiCollectionResult } from 'src/app/models/swapi-collection-result';
 })
 export class SwapiDashboardComponent implements OnInit {
 
-  readonly firstTabResourse = 'people';
+  readonly firstTabResource: string = 'people';
+  isSomeResourceNotAvailable: boolean = false;
 
-  existingSWAPIResoursesToGet: string[];
+  existingSWAPIResourcesToGet: string[] = [];
   people: SwapiPeople[]
 
   constructor(
@@ -23,25 +25,40 @@ export class SwapiDashboardComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.loadDataForFirstTab().subscribe();
+    this.initData();
   }
 
   getRoot(): Observable<SwapiRoot> {
     return new Observable(obs => {
       this.swapi.getRoot().subscribe(res => {
-        this.existingSWAPIResoursesToGet = Object.keys(res);
+        this.existingSWAPIResourcesToGet = Object.keys(res);
         obs.next(res);
+        obs.complete();
       });
     });
   }
 
   loadDataForFirstTab(): Observable<SwapiCollectionResult<SwapiPeople[]>> {
     return new Observable(obs => {
-      this.swapi.getPeople().subscribe(res => {
-        this.people = res.results;
-        console.log(this.people);
-        obs.next(res);
-      })
+      const isPeopleResourceExists: boolean = this.existingSWAPIResourcesToGet.some(res => res === this.firstTabResource);
+      if (isPeopleResourceExists) {
+        this.swapi.getPeople().subscribe(res => {
+          this.people = res.results;
+          obs.next(res);
+          obs.complete();
+        });
+      } else {
+        this.isSomeResourceNotAvailable = true;
+      }
+    });
+  }
+
+  /**
+   * inits data for first tab
+   */
+  initData(): void {
+    const observablesToSubscribe = [this.getRoot(), this.loadDataForFirstTab()];
+    concat(...observablesToSubscribe).subscribe(res => {console.log(res);
     });
   }
 
